6b8ccdbe607fee490377e0f7f3c831d131d450dc
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-05-29 17:12:43 +0200

    Scripts: add JS script and testing

--- frontend/view/contact/contact.js
+++ frontend/view/contact/contact.js
@@ -1,3 +1,8 @@
 /**
  * Gère l'affichage et les interactions de la page de contact
  */
+const currentPage = 'contact';
+
+if (document.body.classList.contains(`${currentPage}`)) {
+    console.log(`Is Page => '${currentPage}'`);
+}
--- frontend/view/favorites/favorites.js
+++ frontend/view/favorites/favorites.js
@@ -1,3 +1,8 @@
 /**
  * Gère l'affichage et les interactions de la page des favoris
  */
+const currentPage = 'favoris';
+
+if (document.body.classList.contains(`${currentPage}`)) {
+    console.log(`Is Page => '${currentPage}'`);
+}
